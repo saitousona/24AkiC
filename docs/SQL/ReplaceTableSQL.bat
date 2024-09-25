5e2d9ec75c366437b9eb03f5a9589703c194c899
masaya <mt23304012@ga.ttc.ac.jp>
2024-09-25 09:51:18 +0900

    テーブル定義を変更。USERS表のuser_idからemail列へ変更。伴いダミーデータ、SQLスクリプトを修正。

--- docs/SQL/ReplaceTableSQL.bat
+++ docs/SQL/ReplaceTableSQL.bat
@@ -5,22 +5,26 @@ set /p DB_PASSWORD=パスワードを入力してください:
 
 set DB_CONNECT=localhost:1521/orcl
 
-set /p selectOracleSQL=Create or Drop or Insert(Enter:c/d/i) 
+set /p selectOracleSQL=Create or Drop or Insert(Enter:c/d/i): 
 
 if "%selectOracleSQL%"=="c" (
     rem Create Table
-    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @C1_create_users.sql
-    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @C2_create_ticket.sql
-) else if "%selectOracleSQL%"=="d" (
-    rem Drop Table
-    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @D1_drop_ticket.sql
-    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @D2_drop_users.sql
-) else if "%selectOracleSQL%"=="i" (
-    rem Insert Table
-    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @I1_insert_users.sql
-    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @I2_insert_ticket.sql
+    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @C1_create_users.sql  >>sql.log
+    sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @C2_create_ticket.sql  >>sql.log
 ) else (
-    rem error process
-    echo mistake
-    pause
+    if "%selectOracleSQL%"=="d" (
+        rem Drop Table
+        sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @D1_drop_ticket.sql  >>sql.log
+        sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @D2_drop_users.sql  >>sql.log
+    ) else (
+        if "%selectOracleSQL%"=="i" (
+            rem Insert Table
+            sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @I1_insert_users.sql  >>sql.log
+            sqlplus "%DB_USER%"/"%DB_PASSWORD%"@%DB_CONNECT% @I2_insert_ticket.sql  >>sql.log
+        ) else (
+            rem error process
+            echo mistake
+            pause
+        )
+    )
 )
