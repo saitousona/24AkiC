d1cef249a9e48002fef848ee807ea08fadbb326d
masaya <mt23304012@ga.ttc.ac.jp>
2024-09-29 16:17:35 +0900

    ユーザー作成機能を実装。

--- src/main/java/ticket/CreateUserServlet.java
+++ src/main/java/ticket/CreateUserServlet.java
@@ -1,5 +1,46 @@
 package ticket;
 
-public class CreateUserServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.CreateUserDAO;
+
+
+public class CreateUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        String email = request.getParameter("email").trim();
+        String userName = request.getParameter("user_name").trim();
+        String password = request.getParameter("password");
+        String secretWord = request.getParameter("secret_word").trim();
+
+        // ユーザーの登録処理
+        CreateUserDAO createUserDAO = new CreateUserDAO();
+
+        try {
+        	
+
+            boolean isUserCreated = createUserDAO.createUser(email, userName, password, secretWord);
+            if (isUserCreated) {
+                request.setAttribute("successMessage", "ユーザーが正常に作成されました。");
+                request.getRequestDispatcher("/WEB-INF/JSP/CreateUserSuccess.jsp").forward(request, response);
+            } else {
+                request.setAttribute("errorMessage", "ユーザーの作成に失敗しました。すでに同じEmailが存在している可能性があります。");
+                request.getRequestDispatcher("/WEB-INF/JSP/CreateUserError.jsp").forward(request, response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("errorMessage", "システムエラーが発生しました。");
+            request.getRequestDispatcher("/WEB-INF/JSP/CreateUserError.jsp").forward(request, response);
+        }
+    }
 }
---
+++ src/main/webapp/WEB-INF/JSP/CreateUserSuccess.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ユーザー作成成功</title>
+</head>
+<body>
+    <h2>ユーザーが正常に作成されました！</h2>
+    <a href="TopPageServlet">ログインページへ</a>
+</body>
+</html>
